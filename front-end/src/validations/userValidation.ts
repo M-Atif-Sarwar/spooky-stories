2c7d9923bfef3ad4a1bd4e59b7a3c0314c2db97d
M Atif Sarwar <matifsarwar32@gmail.com>
2025-11-09 10:15:36 -0800

    update validation

--- front-end/src/validations/userValidation.ts
+++ front-end/src/validations/userValidation.ts
@@ -1,21 +1,18 @@
-import path from 'path';
 import z from 'zod'
 
 const passwordRegex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
+const passmessage="Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character "
+
 export const signupSchema=z.object({
     username:z.string(),
     email:z.string().toLowerCase().trim().includes("@"),
-    password:z.string().min(8).regex(passwordRegex),
-    confirmPaswword:z.string()
-}).refine((data)=>{
-    if(data.password ===data.confirmPaswword ){
-        return {
+    password:z.string().min(8,'Password must 8 character long').regex(passwordRegex,passmessage),
+    confirmPassword:z.string()
+}).refine((data)=>data.password === data.confirmPassword ,{
             message:"password do not match",
-            path:["confirmPassword"]
-        }
-    }
-})
-
+            path:["confirmPassword"],
+        })
+        
 export const loginSchema=z.object({
     email:z.string().toLowerCase().toLowerCase().includes("@"),
     password:z.string().min(8,"Password must contain atleast 8 character")
