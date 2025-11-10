7b65342c968ca6f3e1d29795e5b2d45e6e1280df
M Atif Sarwar <matifsarwar32@gmail.com>
2025-11-10 08:34:42 -0800

    design adjust in formfield

--- front-end/src/components/formInputField.tsx
+++ front-end/src/components/formInputField.tsx
@@ -1,14 +1,14 @@
 "use client"
 import { useState } from "react";
 import { UseFormRegister,FieldValues, Path,FieldError } from "react-hook-form";
-
+import { IoIosEye } from "react-icons/io";
+import { IoIosEyeOff } from "react-icons/io";
 export type FormFieldProps<TFormValues extends FieldValues> = {
   type?: string;                     
   placeholder?: string;              
   name:Path<TFormValues>;           
   register: UseFormRegister<TFormValues>; 
   error?: FieldError;                     
-  valueAsNumber?: boolean;
   label:string            
 };
 
@@ -19,7 +19,6 @@ export function FormFields<TFormValues extends FieldValues> (
     name,
     register,
     error,
-    valueAsNumber,
     label
  }:FormFieldProps<TFormValues> ){
      const [hidePassword,sethidePassword]=useState(false)
@@ -27,18 +26,20 @@ export function FormFields<TFormValues extends FieldValues> (
 
      return <>
    {/* //   retruning Password field if label is password */}
-       { isPasswordField ?  <div className="flex flex-col gap-y-2">
+       { isPasswordField ?  <div className="flex flex-col gap-y-2 relative">
         <label htmlFor={label}>{label} </label>
         <input
         type={hidePassword ? "text": type}
         placeholder={placeholder}
+        className="w-60 rounded-md pl-1"
         {...register(name)}/>
-
+      
         {error && <span className="text-red-800 text-xs"> {error.message}</span>}
-        <button className='text-white border-8' type='button' onClick={(e)=>{
-         sethidePassword(prev=>!prev)}
-        }
-         >showPaswwword</button>
+        <button 
+        className={'text-md  w-fit absolute bottom-[15px] left-[220px] top-9 {i} ${hidPassword ? "hover:"show passsword"}'} type='button' 
+        onClick={(e)=>{sethidePassword(prev=>!prev)}}
+        
+         >{hidePassword ? <IoIosEye /> :  <IoIosEyeOff />}</button>
 
      </div>
                    
@@ -47,6 +48,7 @@ export function FormFields<TFormValues extends FieldValues> (
         <label htmlFor={label}>{label} </label>
         <input
         type={type}
+        className="w-60 rounded-md pl-1"
         placeholder={placeholder}
         {...register(name)}/>
 
